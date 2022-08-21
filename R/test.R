3fe4b1f1c2c78c02326caf9ff1b6d7b2f7cb10f2
ChrisNice89 <christoph.nitz89@gmail.com>
2022-08-21 10:07:35 +0200

    Update

---
+++ R/test.R
@@ -0,0 +1,12 @@
+
+#' Constructor
+#' @param a a
+#' @param b b
+#' @keywords function
+#' @return Object of \code{\link{String}} with methods for ...
+#' @examples result<-test(a,b)
+#' @include zzz.R
+#' @export
+test<-function(a,b){
+  return ("Test")
+}
---
+++ R/zzz.R
@@ -0,0 +1,47 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    paste(
+      "This package ist created,developed and copyrighted by Jacky lessig.",
+      "Interested parties may contact <jacky.lessig@gmail.com>",
+      sep = "\n"
+    ),
+    domain = NULL,
+    appendLF = TRUE
+  )
+}
+
+.onLoad <- function(libname, pkgname) {
+
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/R-dev",
+    devtools.install.args = "",
+    devtools.name = "Jacky",
+    devtools.desc.author = "Jacky Lessig <Jacky.Lessig@gmail.com> [aut, cre]",
+    devtools.desc.license = "No license specified",
+    devtools.desc.suggests = "NULL",
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+
+  if (any(toset))
+    options(op.devtools[toset])
+
+  packages.needed<- c("devtools","readxl","DBI" )#"roxygen2","rmarkdown")
+
+  # helper
+  installer <- function(pkg){
+    #slice matrix by column
+    packages.installed<- installed.packages()[,"Package"]
+    #logical vector
+    packages.found<- pkg %in% packages.installed
+    #vector Indexing using Logical vector
+    packages.not.installed<- pkg[!packages.found]
+    for(p in packages.not.installed) install.packages(p, dependencies = TRUE,upgrade = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+  }
+
+  installer(packages.needed)
+
+  invisible()
+}
