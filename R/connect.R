ee0cd133625d62ef7305008306e1a71f0b8e703b
ChrisNice89 <christoph.nitz89@gmail.com>
2022-08-21 23:21:43 +0200

    update

--- R/connect.R
+++ R/connect.R
@@ -1,4 +1,3 @@
-
 #' connectDB
 #'
 #' @description
@@ -10,30 +9,25 @@
 #' @param pass pass
 #' @keywords function
 #' @return Object of \code{\link{DBI::Connect}} with methods for ...
-#' @examples result<-connectDB()
 #' @include zzz.R
 #' @export
 #'
-#'
+connectDB <- function(user = Sys.getenv("DB_USERNAME"),
+                       pass = Sys.getenv("DB_PASSWORD", rstudioapi::askForPassword())) {
+
+  # Closing the existing connection if already open
+  # Can instead ask the user if they want to restablish connection and stop if they don't
+  if (!(is.null(pkg.db.env$cnn))) {
+    if (DBI::dbIsValid(pkg.db.env$cnn)) {
+      DBI::dbDisconnect(pkg.db.env$cnn)
+    }
+  }
+  pkg.db.env$cnn <- DBI::dbConnect(pkg.db.env$driver, pkg.db.env$path)
+
+  invisible(TRUE)
+  #return (pkg.db.env$cnn)
+}
 
 pkg.db.env <- new.env()
 pkg.db.env$driver <- RSQLite::SQLite()
 pkg.db.env$path <- "sysdata/revenue.db"
-
-
-connectDB <- function(user = Sys.getenv("DB_USERNAME"),
-                       pass = Sys.getenv("DB_PASSWORD", rstudioapi::askForPassword())) {
-
-  if (is.null(pkg.db.env$cnn)) {
-    pkg.db.env$cnn <- DBI::dbConnect(pkg.db.env$driver, pkg.db.env$path)
-  } #else {
-    # Closing the existing connection if already open
-    # Can instead ask the user if they want to restablish connection and stop if they don't
-    #if (DBI::dbIsValid(DB_ENV$cnn)) {
-    #  DBI::dbDisconnect(DB_ENV$cnn)
-    #}
-    #DB_ENV$cnn <- DBI::dbConnect(DB_DRIVER, DB_DBNAME)
- # }
-  invisible(TRUE)
-}
-
