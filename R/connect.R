c86a84d4315929657230d4ac847cc7683219af22
ChrisNice89 <christoph.nitz89@gmail.com>
2022-08-21 23:05:07 +0200

    Update connect.R

--- R/connect.R
+++ R/connect.R
@@ -3,7 +3,7 @@ connectDB <- function(user = Sys.getenv("DB_USERNAME"),
                        pass = Sys.getenv("DB_PASSWORD", rstudioapi::askForPassword())) {
 
   if (is.null(pkg.db.env$cnn)) {
-    pkg.db.env$cnn <- DBI::dbConnect(DB_DRIVER, DB_DBNAME)
+    pkg.db.env$cnn <- DBI::dbConnect(pkg.db.env$driver, pkg.db.env$path)
   } #else {
     # Closing the existing connection if already open
     # Can instead ask the user if they want to restablish connection and stop if they don't
