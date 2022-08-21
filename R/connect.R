d27f45c9ada9d6736062b694f0d0e72bd1bb7076
ChrisNice89 <christoph.nitz89@gmail.com>
2022-08-21 22:39:01 +0200

    Update connect.R

--- R/connect.R
+++ R/connect.R
@@ -4,8 +4,12 @@ DB_DBNAME <- "sysdata/revenue.db"
 
 connectDB <- function(user = Sys.getenv("DB_USERNAME"),
                        pass = Sys.getenv("DB_PASSWORD", rstudioapi::askForPassword())) {
+
+  cat("in connect")
   if (is.null(DB_ENV$cnn)) {
+    cat("in if connect")
     DB_ENV$cnn <- DBI::dbConnect(DB_DRIVER, DB_DBNAME)
+    cat("in if connect complete")
   } else {
     # Closing the existing connection if already open
     # Can instead ask the user if they want to restablish connection and stop if they don't
@@ -14,6 +18,5 @@ connectDB <- function(user = Sys.getenv("DB_USERNAME"),
     }
     DB_ENV$cnn <- DBI::dbConnect(DB_DRIVER, DB_DBNAME)
   }
-
   invisible(TRUE)
 }
